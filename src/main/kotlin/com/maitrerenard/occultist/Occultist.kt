4cc8ac9cce15fddf09880510e5745d425ad9f982
MaitreRenard18 <maitre.renardowo@gmail.com>
2025-05-26 13:51:20 +0200

    First block

--- src/main/kotlin/com/maitrerenard/occultist/Occultist.kt
+++ src/main/kotlin/com/maitrerenard/occultist/Occultist.kt
@@ -1,15 +1,28 @@
 package com.maitrerenard.occultist
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.AbstractBlock
+import net.minecraft.block.Block
+import net.minecraft.item.BlockItem
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
 object Occultist : ModInitializer {
     private val logger = LoggerFactory.getLogger("occultist")
 
+	private val block = Block(AbstractBlock.Settings.create().strength(4.0f))
+	val blockItem = BlockItem(block, Item.Settings())
+
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		Registry.register(Registries.ITEM, Identifier.of("occultist", "test_block"), blockItem)
+		Registry.register(Registries.BLOCK, Identifier.of("occultist", "test_block"), block)
 		logger.info("Hello Fabric world!")
 	}
 }
